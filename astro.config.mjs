0b85ce2a40dd88ed160795aa059d09de164498d4
edocg <eduardocgalvez@gmail.com>
2024-07-06 11:04:32 -0500

    add hybrid render

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,6 @@ export default defineConfig({
   adapter: node({
     mode: "standalone"
   }) */,
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare()
 });
--- src/pages/api/get-person.json.ts
+++ src/pages/api/get-person.json.ts
@@ -1,5 +1,6 @@
 import type {APIRoute} from 'astro';
 
+
 export const GET: APIRoute = async ({params,request})=>{
 
     const person = {
